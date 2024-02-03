4449cbc1cd3a1bba474c9cdc8deb5cc5b5dfb31a
Andras Csanyi <andras.csanyi@pm.me>
2024-02-03 15:39:18 +0100

    Further keymaps

--- nvim/lua/andras/keymaps.lua
+++ nvim/lua/andras/keymaps.lua
@@ -27,5 +27,30 @@ keymap("n", "<C-Down>", ":resize +2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 
+-- Navigate buffers
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
+
 -- NvimTree
 keymap("n", "<leader>e", ":NvimTreeToggle<cr>", opts)
+
+-- -- Insert mode -- --
+-- fast "jk" will leave Insert mode
+keymap("i", "jk", "<ESC>", opts)
+
+-- -- Visual -- --
+-- Indentation increase an decrease
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- Moving text up and down
+keymap("v", "<A-j>", ":m .+1<CR>==", opts)
+keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+keymap("v", "p", '"_dP', opts)
+
+-- -- Visual Block -- --
+-- Move text up and down
+keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "K", ":move '<-1<CR>gv-gv", opts)
+keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
